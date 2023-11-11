1c89a3c812fd4dd1c774588d64bd46ae0b331599
eric-spu <bscnrb591520@spu.ac.ke>
2023-11-11 12:07:57 +0300

    file one

--- tutorial1.py
+++ tutorial1.py
@@ -1,11 +1,15 @@
+# Import the Flask class from the flask module
 from flask import Flask
 
+# Create a Flask web application instance
 app = Flask(__name__)
 
+# Define a route for the root URL ("/") that maps to the home function
 @app.route("/")
 def home():
-    return "Hello! this is the main page <h1>Home</h1>"
+    # Return a simple HTML response as a string
+    return "Hello! This is the main page <h1>Home</h1>"
 
+# Run the Flask application if this script is executed directly
 if __name__ == "__main__":
     app.run()
-    
